84f79d857774256f4df0db2139c28fc0697f3584
CubBossa <leonard.bausenwein@web.de>
2022-06-11 19:06:18 +0200

    EntityFactory

--- GunsApi-Plugin/src/main/java/de/cubbossa/guns/plugin/EntityFactory.java
+++ GunsApi-Plugin/src/main/java/de/cubbossa/guns/plugin/EntityFactory.java
@@ -7,13 +7,16 @@ import de.tr7zw.nbtapi.NBTEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.commons.lang.SerializationException;
 import org.bukkit.Location;
+import org.bukkit.NamespacedKey;
+import org.bukkit.Registry;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
+import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 
 @Getter
@@ -25,7 +28,7 @@ public class EntityFactory {
 	private Map<String, Object> objectRepresentation;
 	private NBTContainer nbtContainer;
 
-	private Function<Location, Entity> spawnFunction;
+	private BiFunction<EntityType, Location, Entity> spawnFunction;
 	private Location location;
 
 	public EntityFactory(EntityType entityType, Map<String, Object> objectRepresentation, NBTContainer nbtContainer) {
@@ -34,7 +37,7 @@ public class EntityFactory {
 		this.nbtContainer = nbtContainer;
 	}
 
-	public Entity spawnEntity(Location location, Function<Location, Entity> spawnFunction) {
+	public Entity spawnEntity(Location location, BiFunction<EntityType, Location, Entity> spawnFunction) {
 		this.location = location;
 		this.spawnFunction = spawnFunction;
 		return fromNBT(nbtContainer);
@@ -47,9 +50,20 @@ public class EntityFactory {
 	 * e.g. {id:"minecraft:donkey",Passengers:[{id:"minecraft:wolf",Passengers:[{id:"minecraft:cat"}]}]}
 	 */
 	private Entity fromNBT(NBTCompound nbtContainer) {
+		System.out.println(nbtContainer.toString());
+		String keyString = nbtContainer.getString("id");
+		if (keyString == null) {
+			throw new SerializationException("Could not summon entity, no 'id' attribute provided.");
+		}
+		NamespacedKey key = NamespacedKey.fromString(keyString);
+		if (key == null) {
+			throw new SerializationException("Could not summon entity, invalid 'id' attribute: " + keyString);
+		}
+		EntityType type = Registry.ENTITY_TYPE.get(key);
+
 		NBTCompoundList passengersCompound = nbtContainer.getCompoundList("Passengers");
 		List<Entity> passengers = passengersCompound.stream().map(this::fromNBT).collect(Collectors.toList());
-		Entity entity = spawnFunction.apply(location);
+		Entity entity = spawnFunction.apply(type, location);
 		NBTEntity nbtEntity = new NBTEntity(entity);
 		nbtEntity.mergeCompound(nbtContainer);
 		passengers.forEach(entity::addPassenger);
